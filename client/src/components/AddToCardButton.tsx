7525b29e12dc8ac4c4e4d0807070ca468ee88c2a
Kaisa Evikoski <kaisa.evikoski@gmail.com>
2024-04-02 21:02:45 +0200

    Add addtocardbutton, loginbutton

---
+++ client/src/components/AddToCardButton.tsx
@@ -0,0 +1,10 @@
+
+const AddToCardButton = () => {
+  return (
+    <>
+      <button>Add to Card</button>
+    </>
+  )
+}
+
+export default AddToCardButton
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import Products from "../components/Products";
+
 const HomePage = () => {
   return (
     <div>
       <h3>Shop your furnitures here</h3>
+      <Products />
     </div>
   );
 };
